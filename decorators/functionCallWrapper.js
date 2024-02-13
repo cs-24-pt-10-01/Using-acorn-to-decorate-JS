1c23a02f723471bec91de124619a53c7a43bf416
Villiamfj <villiamfj@hotmail.dk>
2024-02-13 11:14:11 +0100

    Added support for function calls in return statements

--- decorators/functionCallWrapper.js
+++ decorators/functionCallWrapper.js
@@ -27,13 +27,52 @@ function DecorateBlock(body, startNode, stopNode) {
                 }
             }
         }
+        // return statement with function call
+        else if (innerNode.type == "ReturnStatement" && innerNode.argument.type == "CallExpression") {
+            toChange.push({ index, innerNode: innerNode });
+        }
+
     });
     let i = 0; // counter to keep added nodes into count
     toChange.forEach((change) => {
-        // Adding nodes
-        body.splice(change.index + i, 0, startNode);
-        body.splice(change.index + 2 + i, 0, stopNode);
-        i += 2;
+        // if the node is a return statement, the function call is moved to a variable and then returned
+        if (change.innerNode.type == "ReturnStatement") {
+            // variable with the function call
+            const variableNode = {
+                type:"VariableDeclaration",
+                declarations: [
+                    {
+                        type: "VariableDeclarator",
+                        id: {
+                            type: "Identifier",
+                            name: "__result"
+                        },
+                        init: change.innerNode.argument
+                    }
+                ],
+                kind: "const"
+            }
+
+            // Return node for returning the result
+            //const returnNode = acorn.parse("return __result").body[0];
+            const returnNode = {
+                type: "ReturnStatement",
+                argument: {
+                    type: "Identifier",
+                    name: "__result"
+                }
+            }
+
+            // replacing the return statement with new nodes
+            body.splice(change.index + i, 1, startNode, variableNode, stopNode, returnNode);
+            i += 3;
+        }
+        else {
+            // Adding nodes
+            body.splice(change.index + i, 0, startNode);
+            body.splice(change.index + 2 + i, 0, stopNode);
+            i += 2;
+        }
     });
 }
 
