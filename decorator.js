f85b5ba1ccab89bf2ff8a23818b41aca9e0b4bd7
Villiam <Vjacob19@student.aau.dk>
2024-02-15 15:25:09 +0100

    Changed main.js to decorator.js meant to be used as library

---
+++ decorator.js
@@ -0,0 +1,40 @@
+import * as fs from 'fs';
+import * as acorn from 'acorn'
+import { toJs } from 'estree-util-to-js'
+import { decorateBrackets } from './decorators/brackets.js';
+import { findFunctionCallsInAST, wrapFunctions } from './decorators/functionCallWrapper.js';
+
+// reads a single file and returns a decorated version as a string
+function decorateString(code){
+    const ast = acorn.parse(code, { ecmaVersion: 2022 });
+
+    // rapl start and stop nodes
+    const startNode = acorn.parse("rapl.start()").body[0];
+    const stopNode = acorn.parse("rapl.stop()").body[0];
+
+    // wrap function calls in start and stop nodes
+    wrapFunctions(ast, startNode, stopNode);
+
+    return toJs(ast).value;
+}
+
+function decorateSingleFile(path){
+    const code = readFileSync(path);
+    const decoratedCode = decorateString(code);
+    fs.writeFile(path + "_decorated.js", decoratedCode, (err) => {
+        if (err) throw err;
+    });
+}
+
+function decorateFolder(path){
+    fs.readdirSync(path);
+    throw new error("Not implemented")
+}
+
+function findFunctionCalls(path){
+    const code = fs.readFileSync(path);
+    const ast = acorn.parse(code, { ecmaVersion: 2022, locations: true});
+    return findFunctionCallsInAST(ast);
+}
+
+export { decorateString, decorateSingleFile, decorateFolder, findFunctionCalls };
