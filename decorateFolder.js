4528017124c38cb711617c7feaf8ec7a2c80742e
Villiam <Vjacob19@student.aau.dk>
2024-04-01 14:05:11 +0200

    Linux path fix for decorateFolder

--- decorateFolder.js
+++ decorateFolder.js
@@ -2,7 +2,7 @@ import * as decorator from "./decorator.js"
 import os from 'os';
 
 const path = process.argv[2];
-const projektPath = process.argv[1].split("\\").slice(0, -1).join("\\");
+const projektPath = process.argv[1].split("/").slice(0, -1).join("/");
 const libEnd = os.platform() == "win32" ? "dll" : "so";
 
 decorator.decorateFolder(path
