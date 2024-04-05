833f1eb57fac97983c73ecf0a070ade5226862f0
Villiam <Vjacob19@student.aau.dk>
2024-04-05 09:45:38 +0200

    seperators dependent on OS

--- decorateFolder.js
+++ decorateFolder.js
@@ -2,11 +2,15 @@ import * as decorator from "./decorator.js"
 import os from 'os';
 
 const path = process.argv[2];
-const projektPath = process.argv[1].split("/").slice(0, -1).join("/");
+const seperator = os.platform() == "win32" ? "\\" : "/";
+const projektPath = process.argv[1].split(seperator).slice(0, -1).join(seperator);
 const libEnd = os.platform() == "win32" ? "dll" : "so";
+
+const body = process.argv[3] == "true";
 
 decorator.decorateFolder(path
     , projektPath + "/libs/rapl.js"
-    , projektPath + "/libs/" + "thor_local_client." + libEnd);
+    , projektPath + "/libs/" + "thor_local_client." + libEnd
+    , body);
 
 console.log("folder decorated");
